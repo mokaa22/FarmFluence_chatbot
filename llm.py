174cab8a482b7964aeac5f34819e2ffb6c5a334a
mokaa22 <mokshabhandari100@gmail.com>
2026-08-20 17:43:37 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -8,12 +8,13 @@ if not GROQ_API_KEY:
 
 client = Groq(api_key=GROQ_API_KEY)
 
+
 def ask_llm(messages):
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="qwen/qwen3-32b",
         messages=messages,
-        temperature=0.3,
-        max_completion_tokens=4096
+        temperature=0.5,
+        max_tokens=4096
     )
 
     return response.choices[0].message.content
